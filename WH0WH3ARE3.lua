37f7320b011ec9cdfe4c0b410250c126128570c9
WHOAMI <sp4rk3r444@gmail.com>
2025-12-21 21:04:29 -0800

    Update version number to v1.4
    Updated version from v1.3 to v1.4 in comments.

--- WH0WH3ARE3.lua
+++ WH0WH3ARE3.lua
@@ -1,7 +1,7 @@
 -- 🦈 Glassmas UI • Principal (Apple Glass Christmas) • Single Script 
 -- ✅ FIXED • NO "Label" VACÍO • UI COMPLETA • XENO READY
 -- Made for Sp4rk 💎
---v1.3
+--v1.4
 
 --==================== SERVICES ====================
 local Players = game:GetService("Players")
