dd4c8a8410aae03ae8fb4b3dceadc7b42d1dee6b
WHOAMI <212384818+Who1amG@users.noreply.github.com>
2026-01-12 06:40:44 -0700

    Update WH0WH3ARE3.lua

--- WH0WH3ARE3.lua
+++ WH0WH3ARE3.lua
@@ -1,9 +1,3 @@
--- 🦈 Glassmas UI • Principal (Apple Glass Christmas) • Single Script
--- ✅ FIXED • NO "Label" VACÍO • UI COMPLETA • XENO READY
--- Made for Sp4rk 💎
---v2.2
---fixes 90%
--- 91% working
 --==================== SERVICES ====================
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
